1d2f002144bfdcf086103afe478af96fc1df3496
Sebastian Heusinger <heusings@googlemail.com>
2019-10-26 20:34:02 -0700

    added stoppage condition and centroid selection

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,13 @@
-
 import sys
 import pandas as pd
 import random as rand
+import numpy as np
 import math
+
+
+class KMeans:
+    def __init__ (self):
+        pass
 
 def diskKMeans(data, k):
     centroids = selectInitialCentroids(data, k)
@@ -43,16 +48,57 @@ def get_dist(test_point, train_point):
 
     return distance
 
+# euclidean distance of two or more points(array)
+def calc_distance(p1, p2):
+    x = np.sqrt(np.sum(np.power(p1-p2, 2), axis=1))
+    return x
 
-# stoppage conditions
-def done(centroids, k):
-    return False
+# stoppage condition 3
+# sums all point distances to their respective centroid
+# check if working
+# prototype
+def stoppage_sse(self, clusters, centroids, threshhold):
+    sse = 0
+    for i, c in enumerate(clusters):
+        for p in c:
+            sse += calc_distance(p, centroids[i])
+    return abs(sse - self.prev_sse) < threshhold
 
-# making this random for now
+# stoppage condition 2
+# prototype
+def centroids_changed(self, centroids, threshold):
+    changed = False
+    for i,c in enumerate(centroids):
+        if abs(np.sum(c) - np.sum(self.prev_centroids[i])) > threshold:
+            changed = True
+    return changed
+# stoppage condition 1
+# protoype
+def check_reasignments(self, clusters, threshold):
+    changed = False
+    for i, cl in enumerate(clusters):
+        if abs(len(cl) - self.prev_len[i]) > threshold:
+            changed = True
+    return changed
+
+def done(self, clusters, centroids, centroid_th, cluster_th, sse_th):
+    return self.check_reasignments(clusters, cluster_th) or self.centroids_changed(centroids, centroid_th) or stoppage_sse(clusters, centroids, sse_th)
+
+#find the centroid of the complete dataset
+def find_dataset_centroid(data):
+    return np.sum(data)/len(data)
+
+# check if this makes sense
+# should work
 def selectInitialCentroids(data, k):
     centroids = []
+    centroid = find_dataset_centroid(data)
+    centroid_distances = calc_distance(data, centroid)
     for i in range(k):
-        centroids.append(rand.choice(data.values.tolist()))
+        index_centroid = np.argmax(centroid_distances)
+        centroid = data.iloc[index_centroid]
+        centroids.append(centroid)
+        centroid_distances += calc_distance(data, centroid)
     return centroids
 
 
@@ -64,6 +110,7 @@ def main():
     else:
         filename = sys.argv[1]
     data = pd.read_csv(filename)
+    data_wo_cl = data.drop(data.columns[4], axis = 1)
     k = 3
     print(diskKMeans(data, k))
 
