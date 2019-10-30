6ee50814d3a464b5d47d0c5960c730a77b8561fd
Sebastian Heusinger <heusings@googlemail.com>
2019-10-29 17:39:15 -0700

    kmeans update

--- kmeans.py
+++ kmeans.py
@@ -9,31 +9,15 @@ class KMeans:
     def __init__ (self):
         pass
 
-<<<<<<< HEAD
     def diskKMeans(self, data, k):
         attr_dict = self.get_attr_dict(data)
     # TODO re-organize so that is works with initial conditions
         data = data.iloc[:, :-1]
         centroids = self.selectInitialCentroids(data, k)
-=======
-def diskKMeans(data, k):
-    data = data.iloc[:, :-1]
-    centroids = selectInitialCentroids(data, k)
-    clusters = [[] for i in range(k)]
-    # arrange all the points into clusters
-    for i in range(len(data)):
-        cl_index = closest_cluster_index(data.iloc[i], centroids)
-        clusters[cl_index].append(data.iloc[i])
-    while(not done(centroids, clusters)):
-        # compute new centroids
-        for i in range(k):
-            centroids[i] = average_point(clusters[i])
->>>>>>> 6bb4bfb124985f8a49f1c6c7c7caeeecf900d722
         clusters = [[] for i in range(k)]
         for i in range(len(data)):
             cl_index = self.closest_cluster_index(data.iloc[i], centroids)
             clusters[cl_index].append(data.iloc[i])
-<<<<<<< HEAD
         while(not self.done(centroids, clusters)):
             self.prev_centroids = centroids
                 # each index in clusters represents the number of its centroid
@@ -41,8 +25,6 @@ def diskKMeans(data, k):
             # arrange all the points into clusters
             for i in range(k):
                 centroids[i] = self.average_point(clusters[i], attr_dict)
-=======
->>>>>>> 6bb4bfb124985f8a49f1c6c7c7caeeecf900d722
 
             clusters = [[] for i in range(k)]
             for i in range(len(data)):
@@ -53,40 +35,10 @@ def diskKMeans(data, k):
 
 
 
-<<<<<<< HEAD
-
-    def average_point(self, cluster, attr_dict):
-        average_point = []
-        for data_point in cluster:
-            for attr in data_point:
-                if(str(attr).isdigit()):
-                    attr_dict[attr] += attr_dict[attr]
-                else:
-                    attr_dict[attr].append(attr)
-
-        i = 0
-        for k,v in attr_dict:
-            if(type(v) == list):
-                average_point[i] = max(set(v), key=v.count)
-            else:
-                average_point[i] = v / len(cluster)
-            i += 1
+    def average_point(self, cluster):
+        average_point = np.average([dp.to_numpy() for dp in cluster], axis=0)
 
         return pd.Series(data=average_point)
-
-
-    def get_attr_dict(self, data):
-        attr_dict = {}
-        attrs = list(data.columns[:-1])
-
-        for attr in attrs:
-            if(str(attr).isdigit()):
-                attr_dict[attr] = 0
-            else:
-                attr_dict[attr] = []
-
-        return attr_dict
-
 
 
     def closest_cluster_index(self, data_point, centroids):
@@ -163,89 +115,6 @@ def diskKMeans(data, k):
         centroids = []
         centroid = self.find_dataset_centroid(data)
         centroid_distances = self.calc_distance(data, centroid)
-=======
-def average_point(cluster):
-    average_point = np.average([dp.to_numpy() for dp in cluster], axis=0)
-
-    return pd.Series(data=average_point)
-
-
-def closest_cluster_index(data_point, centroids):
-    min_dist = math.inf
-    min_centroid = None
-    min_index = -1
-
-    for i in range(len(centroids)):
-        dist = get_dist(data_point, centroids[i])
-        if(dist < min_dist):
-            min_dist = dist
-            min_centroid = centroids[i]
-            min_index = i
-
-    return min_index
-
-
-def get_dist(test_point, train_point):
-    distance = 0
-    euc_distance = 0
-    for i in range(len(train_point)):
-        if(type(train_point[i]) is str):
-            if(test_point[i] != train_point[i]):
-                distance += 1
-        else:
-            euc_distance += pow((test_point[i] - train_point[i]), 2)
-    distance += math.sqrt(euc_distance)
-
-    return distance
-
-# euclidean distance of two or more points(array)
-def calc_distance(p1, p2):
-    x = np.sqrt(np.sum(np.power(p1-p2, 2), axis=1))
-    return x
-
-# stoppage condition 3
-# sums all point distances to their respective centroid
-# check if working
-# prototype
-def stoppage_sse(self, clusters, centroids, threshhold):
-    sse = 0
-    for i, c in enumerate(clusters):
-        for p in c:
-            sse += calc_distance(p, centroids[i])
-    return abs(sse - self.prev_sse) < threshhold
-
-# stoppage condition 2
-# prototype
-def centroids_changed(self, centroids, threshold):
-    changed = False
-    for i,c in enumerate(centroids):
-        if abs(np.sum(c) - np.sum(self.prev_centroids[i])) > threshold:
-            changed = True
-    return changed
-# stoppage condition 1
-# protoype
-def check_reasignments(self, clusters, threshold):
-    changed = False
-    for i, cl in enumerate(clusters):
-        if abs(len(cl) - self.prev_len[i]) > threshold:
-            changed = True
-    return changed
-
-def done(self, clusters, centroids, centroid_th, cluster_th, sse_th):
-    return self.check_reasignments(clusters, cluster_th) or self.centroids_changed(centroids, centroid_th) or stoppage_sse(clusters, centroids, sse_th)
-
-#find the centroid of the complete dataset
-def find_dataset_centroid(data):
-    return np.sum(data)/len(data)
-
-# check if this makes sense
-# should work
-def selectInitialCentroids(data, k):
-    centroids = []
-    centroid = find_dataset_centroid(data)
-    centroid_distances = calc_distance(data, centroid)
-    for i in range(k):
->>>>>>> 6bb4bfb124985f8a49f1c6c7c7caeeecf900d722
         index_centroid = np.argmax(centroid_distances)
         centroid = data.iloc[index_centroid]
         centroids.append(centroid)
